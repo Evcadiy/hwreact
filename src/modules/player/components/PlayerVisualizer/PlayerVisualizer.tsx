43d748ec1c3d1215730848ea63e60dc94381fda1
Evcadiy <nikusha1305@gmail.com>
2025-02-16 16:16:11 +0200

    Optimize rendering: memoize PlayerVisualizer

--- src/modules/player/components/PlayerVisualizer/PlayerVisualizer.tsx
+++ src/modules/player/components/PlayerVisualizer/PlayerVisualizer.tsx
@@ -1,7 +1,11 @@
-export const PlayerVisualizer = ({
-  waveformRef
-}: {
-  waveformRef: React.RefObject<HTMLDivElement | null>;
-}) => {
-  return <div ref={waveformRef} className="w-full h-[250px]" />;
-};
+import { memo } from "react";
+
+export const PlayerVisualizer = memo(
+  ({
+    waveformRef
+  }: {
+    waveformRef: React.RefObject<HTMLDivElement | null>;
+  }) => {
+    return <div ref={waveformRef} className="w-full h-[250px]" />;
+  }
+);
